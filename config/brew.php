e2702ddd68469d3685bd966e33b126d447b089c1
Vladimir Nikolic <vladimir@codingwisely.com>
2025-08-07 17:18:29 +0200

    Fix config file structure with token and sandbox settings

--- config/brew.php
+++ config/brew.php
@@ -1,6 +1,25 @@
 <?php
 
-// config for CodingWisely/LaravelBrew
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Terminal Shop API Token
+    |--------------------------------------------------------------------------
+    |
+    | Your Terminal Shop API token for authentication.
+    | Get your token from: https://terminal.shop
+    |
+    */
+    'token' => env('TERMINAL_SHOP_TOKEN'),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Use Sandbox Mode
+    |--------------------------------------------------------------------------
+    |
+    | When enabled, all requests will be sent to the Terminal Shop sandbox API
+    | instead of the production API. Useful for testing.
+    |
+    */
+    'sandbox' => env('TERMINAL_SHOP_SANDBOX', false),
 ];
